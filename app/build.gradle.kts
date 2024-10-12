dec1b210fe60d98ac87de59b9dbfdfabed10a1ff
JSenpa1 <41582843+JSenpa1@users.noreply.github.com>
2024-10-13 00:09:20 +0700

    tes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.biometric:biometric:1.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
